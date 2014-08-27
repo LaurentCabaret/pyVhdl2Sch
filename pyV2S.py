4961967ab70fc33361954314553613fe6e8b4851
Laurent cabaret <laurent.cabaret@ecp.fr>
2014-08-27 13:58:58 +0200

    Add a demo mode : if no vhdl file is given the demo one is used datas/test_files/demo.vhd

--- pyV2S.py
+++ pyV2S.py
@@ -7,11 +7,13 @@ from decorator.pdfdrawer import PdfDrawer
 
 import sys
 
-print 'Number of arguments:', len(sys.argv), 'arguments.'
-print 'Argument List:', str(sys.argv)
-
-filename = str(sys.argv[1])
+try:
+	filename = str(sys.argv[1])
+except:
+	filename = "datas/test_files/demo.vhd"
 
 reader = Vhdl_reader(filename)
 
 drawer = PdfDrawer("%s.pdf" % reader.entity.name, reader.entity)
+
+print "The schematic was generated and named : %s.pdf" % reader.entity.name
